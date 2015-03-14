0bbc384fcc57c9c193b346ed0bb07e57d57e3727
Andreas Simon <a.simon@quagilis.de>
2015-03-14 12:43:21 +0100

    Start MySQL and Redis databases and link them into the application container

--- bin/run-app.sh
+++ bin/run-app.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
+docker run --detach \
+    --name magento-mysql \
+    --env MYSQL_ROOT_PASSWORD=root \
+    mysql
+docker run --detach \
+    --name magento-redis \
+    redis
 docker run --rm -ti \
     --publish 8080:80 \
+    --link magento-mysql:mysql \
+    --link magento-redis:redis \
     --volume `pwd`/app:/var/www/html \
     ndrssmn/apache-php5

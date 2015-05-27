bd9404ce7c72a80a31aba1fdb10d29852c2401b5
Andreas Simon <a.simon@quagilis.de>
2015-05-27 16:29:46 +0200

    Added SOLR link

--- bin/run-app.sh
+++ bin/run-app.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
+set -e
 docker run --detach \
     --name magento-mysql \
     --env MYSQL_ROOT_PASSWORD=root \
-    mysql
+    mysql || docker start magento-mysql
 docker run --detach \
     --name magento-redis \
-    redis
+    redis || docker start magento-redis
+docker run --detach \
+    --name magento-solr \
+    makuk66/docker-solr || docker start magento-solr
 docker run --rm -ti \
     --publish 8080:80 \
     --link magento-mysql:mysql \
     --link magento-redis:redis \
+    --link magento-solr:solr \
     --volume `pwd`/app:/var/www/html \
     ndrssmn/apache-php5
